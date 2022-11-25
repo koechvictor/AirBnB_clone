06f27d2dee8a088e6e9391a26ce2d99d2473a7b8
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-11-25 01:11:38 -0800

    update

--- console.py
+++ console.py
@@ -3,7 +3,7 @@
 import cmd
 import ast
 from datetime import datetime as dt
-from models.__init__import storage
+from models.__init__ import storage
 from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.city import City
@@ -13,17 +13,19 @@ from models.state import State
 from models.user import User
 
 classes = {"BaseModel": BaseModel,
-           "Aminity": Amenity,
+           "Amenity": Amenity,
            "City": City,
            "Place": Place,
            "Review": Review,
            "State": State,
            "User": User}
 
+
 class HBNBCommand(cmd.Cmd):
-     """ entry point of the command interpreter """
-    
+    """ entry point of the command interpreter """
+
     prompt = "(hbnb) "
+
     def do_quit(self, args):
         """ Quits the program """
         return True
@@ -36,33 +38,33 @@ class HBNBCommand(cmd.Cmd):
         """ Reads EOF and exits """
         return True
 
-        def do_create(self,args):
-            """ creates a new instance of BaseModel """
-            if not args:
-                print ("** class name missing **")
-            elif args in classes.keys():
-                new = classes[args]()
-                new.save()
-                storage.reload()
-                print(new.id)
-            else:
-                print("** class doesn't exist **")
+    def do_create(self, args):
+        """ creates a new instance of BaseModel """
+        if not args:
+            print("** class name missing **")
+        elif args in classes.keys():
+            new = classes[args]()
+            new.save()
+            storage.reload()
+            print(new.id)
+        else:
+            print("** class doesn't exist **")
 
-          def do_show(self, args):
-              """ shows instances based on class and id
-              args comes in as two values class and id (class ID> """
-              if not args:
-                  print("** class name missing **")
-                  return
-              args = args.split()
-              objs = storage.all()
-              if not args[0] in classes.keys():
-                    print("** class doesn't exist **")
-              elif len(args) < 2:
-                  print("** instance id missing **")
-              else:
-                  for key in objs.keys():
-                       if args[1] in key.split(".")[1]:
-                           print(objs[key])
-                           return
-                       print("** no instance found **")
+    def do_show(self, args):
+        """ Shows instance based on class and id
+        args comes in as two values class and id <class ID> """
+        if not args:
+            print("** class name missing **")
+            return
+        args = args.split()
+        objs = storage.all()
+        if not args[0] in classes.keys():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            for key in objs.keys():
+                if args[1] in key.split(".")[1]:
+                    print(objs[key])
+                    return
+            print("** no instance found **")
