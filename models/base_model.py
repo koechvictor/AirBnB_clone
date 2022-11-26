8e06786cfbe3ab06f222452128a880a071d93ac9
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-11-25 23:25:51 -0800

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
-""" Class BaseModel defines common attributes/methods for other classes"""
+""" Class BaseModel defines common attributes/methods for other classes """
 
 import uuid
 from datetime import datetime as dt
+from models import storage
+
 
 class BaseModel:
-    """ Class BaseModel"""
+    """ Class BaseModel """
 
-    def __init__(self, *ags, **kwargs):
-        """initalize a new instance of BaseModel class
-        using argments and keyword arguments """
-        
+    def __init__(self, *args, **kwargs):
+        """ Initilize a new instance of BaseModel class
+        using arguments and keyword arguments """
+
         if kwargs and kwargs != []:
-            valuei = kwargs["created_at"]
+            value = kwargs["created_at"]
             self.id = kwargs["id"]
+            self.created_at = dt.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
             self.updated_at = self.created_at
         else:
+            self.id = str(uuid.uuid4())
             self.created_at = dt.now()
             self.updated_at = dt.now()
             storage.new(self)
 
-     def __str__(self):
-         """ prints string representing the class 
-         [<class name>] (<self.id>) <self.__dict__> """
-         return ("[{}] ({}) {}".format
-                 (type(self).__name__, self.id, self.__dict__))
+    def __str__(self):
+        """ Prints string represneting the class
 
-     def save(self):
-        """ updates public instance attribute update_at with current datetime """
+        [<class name>] (<self.id>) <self.__dict__> """
+        return ("[{}] ({}) {}".format
+                (type(self).__name__, self.id, self.__dict__))
+
+    def save(self):
+        """ Updates public instance attribute update_at
+        with current datetime"""
+
         self.updated_at = dt.now()
         storage.save()
 
@@ -35,6 +42,7 @@ class BaseModel:
         """ Returns dictionary containing all the keys/values """
 
         __dict__ = dict(self.__dict__)
+        __dict__['__class__'] = type(self).__name__
         __dict__['created_at'] = self.created_at.isoformat()
         __dict__['updated_at'] = self.updated_at.isoformat()
         return __dict__
