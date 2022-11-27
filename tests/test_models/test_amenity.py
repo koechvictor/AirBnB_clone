043dedcabe68e7a7c57750ac63d3eb25812ec35e
LoloDum <chinedumokamgba@gmail.com>
2022-11-27 09:41:38 -0800

    test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-""" Tests for class Amenity """
-
+"""Test suite for Amenity class of the models.amenity module"""
 import unittest
+
+from models.base_model import BaseModel
 from models.amenity import Amenity
-from tests.test_models.test_base_model import TestBaseModel
 
 
-class TestAmenity(TestBaseModel):
-    """ Tests for class Amenity """
+class TestAmenity(unittest.TestCase):
+    """Test cases for the Amenity class"""
 
-    def __init__(self, *args, **kwargs):
-        """ Inititialize models to test """
+    def setUp(self):
+        self.amenity = Amenity()
 
-        super().__init__(*args, **kwargs)
-        self.test_class = Amenity
-        self.test_name = 'Amenity'
+    def test_amenity_is_a_subclass_of_basemodel(self):
+        self.assertTrue(issubclass(type(self.amenity), BaseModel))
+
+    def test_attr_is_a_class_attr(self):
+        self.assertTrue(hasattr(self.amenity, "name"))
+
+    def test_class_attr(self):
+        self.assertIs(type(self.amenity.name), str)
+        self.assertFalse(bool(getattr(self.amenity, "name")))
