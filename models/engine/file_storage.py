3499eb8332f7a7e143208704c8082e7871941139
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-11-27 13:21:14 -0800

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,77 +1,61 @@
-#!usr/bin/python3
-""" My file storage module """
+#!/usr/bin/python3
+""" Class FileStorage the serializes instances to a JSON file
+and deserializes JSON file to instances"""
 import json
 import os
-from pathlib import Path
-
-BASE_DIR = Path(os.getcwd()).resolve()
 
 
 class FileStorage:
-    """ The file storage class """
-    __file_path = str(BASE_DIR / "file.json")
+    """ Class FileStorage """
+
+    __file_path = 'file.json'
     __objects = {}
 
-    def __init__(self):
-        """Initialization method"""
-        pass
-
     def all(self):
-        """ Returns all of the objects"""
-        return FileStorage.__objects
+        """ Returns the dictionary __objects """
+        return type(self).__objects
 
     def new(self, obj):
-        """ Sets a new object to the object dict """
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        """ Sets in __objects the obj with key <obj class name>.id """
+
+        FileStorage.__objects[type(obj).__name__ + "." + obj.id] = obj
 
     def save(self):
-        """ serializes __objects to the JSON file (path: __file_path) """
-        save_dict = {}
-        if FileStorage.__objects:
-            for key, value in FileStorage.__objects.items():
-                save_dict[key] = value.to_dict()
-
-            with open(FileStorage.__file_path, "w") as f:
-                json.dump(save_dict, f)
-        else:
-            with open(FileStorage.__file_path, "w") as f:
-                json.dump({}, f)
+        """ Serializes __objects to the JSON file __file_path """
+        dict_obj = {}
+        for key, value in FileStorage.__objects.items():
+            dict_obj[key] = value.to_dict()
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(dict_obj, f)
 
     def reload(self):
-        """
-        deserializes the JSON file to __objects
-        (only if the JSON file (__file_path) exists;
-        otherwise, do nothing.
-        """
-        if Path(FileStorage.__file_path).exists():
-            with open(FileStorage.__file_path, "r") as r:
-                try:
-                    reload_dict = json.load(r)
-                except:
-                    reload_dict = None
+        """ Deserializes the JSON file to __objects if __file_path exists
+        otherwise do nothing, with no exception being reaised """
+        # if file doesnt exists it returns
+        file_name = FileStorage.__file_path
+        if (not os.path.exists(file_name)) or os.stat(file_name).st_size == 0:
+            return
+        from models.base_model import BaseModel
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
+        from models.user import User
 
-                if reload_dict:
-                    from models.base_model import BaseModel
-                    from models.user import User
-                    from models.state import State
-                    from models.city import City
-                    from models.place import Place
-                    from models.review import Review
-                    from models.amenity import Amenity
-                    FileStorage.__objects = {}
-                    for key, value in reload_dict.items():
-                        if key.startswith("BaseModel"):
-                            FileStorage.__objects[key] = BaseModel(**value)
-                        if key.startswith("User"):
-                            FileStorage.__objects[key] = User(**value)
-                        if key.startswith("State"):
-                            FileStorage.__objects[key] = State(**value)
-                        if key.startswith("City"):
-                            FileStorage.__objects[key] = City(**value)
-                        if key.startswith("Place"):
-                            FileStorage.__objects[key] = Place(**value)
-                        if key.startswith("Review"):
-                            FileStorage.__objects[key] = Review(**value)
-                        if key.startswith("Amenity"):
-                            FileStorage.__objects[key] = Amenity(**value)
+        classes = {"BaseModel": BaseModel,
+                   "Amenity": Amenity,
+                   "City": City,
+                   "Place": Place,
+                   "Review": Review,
+                   "State": State,
+                   "User": User}
+        with open(FileStorage.__file_path, "r") as f:
+            thing = json.load(f)
+        for key, value in thing.items():
+            if value['__class__'] in classes.keys():
+                value = classes[key.split(".")[0]](**value)
+                FileStorage.__objects.update({key: value})
+            else:
+                print("** class doesn't exist **")
+                FileStorage.__objects.update({key: None})
